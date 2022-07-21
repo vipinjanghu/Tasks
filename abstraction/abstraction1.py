9f36b18c2d89501a5a75aa2b4a354c17272d82a9
vipinjanghu <87582760+vipinjanghu@users.noreply.github.com>
2022-07-21 14:59:14 +0530

    first

---
+++ abstraction/abstraction1.py
@@ -0,0 +1,19 @@
+"""Here we are retricting user to _rollandbranch method into other .py files ,so we can say its an example of abstraction"""
+
+import logging
+logging.basicConfig(filename="inheritance3.log",level=logging.INFO,format='%(levelname)s %(asctime)s %(name)s %(message)s')
+
+
+class student:
+    logging.info("we are into parent class")
+    def __init__(self,name,roll,branch):
+        self._name=name
+        self._roll=roll
+        self._branch=branch
+    def _rollandbranch(self):
+        logging.info(f"roll no is : {self._roll} and branch is {self._branch}")
+
+
+
+
+
