105ccf601ac5c88622a5e4abbd6ee7a2c60d0e78
vipinjanghu <87582760+vipinjanghu@users.noreply.github.com>
2022-08-02 16:49:48 +0530

    new sql

--- oops2/mysql/sqlconnector.py
+++ oops2/mysql/sqlconnector.py
@@ -1,5 +1,8 @@
 import mysql.connector as conn
+import pandas as pd
 
 mydb=conn.connect(host="localhost",user ="root",passwd="9971372013")
 cursor=mydb.cursor()
-cursor.execute("create database vipin ")
+cursor.execute("create database if not exists bank;create table if not exists panakj ("
+               "RESTAURANT_SERIAL_NUMBER varchar(30),CLASSIFIER_PROBABILITY float,CLASSIFIER_PREDICTION int)")
+
